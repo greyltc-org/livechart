3a649f7935a2bf3f8155947a32c0667d56402888
Greyson Christoforo <grey@christoforo.net>
2021-08-10 10:41:59 +0100

    switch to black

--- src/livechart/lib.py
+++ src/livechart/lib.py
@@ -2,124 +2,126 @@ import random
 
 
 class Datagetter(object):
-  """
-  gets one data point at a time
-  """
-  _dtype = None  # "random" or "thermal"
-  _zone = None
-  _entered = None
-  temp_file_name = None
-  temp_file_object = None
+    """
+    gets one data point at a time
+    """
 
-  def __init__(self, dtype='random', zone=1):
-    self.zone = zone
-    self.dtype = dtype
+    _dtype = None  # "random" or "thermal"
+    _zone = None
+    _entered = None
+    temp_file_name = None
+    temp_file_object = None
 
-  def __enter__(self):
-    self._entered = True
-    if self._dtype == 'thermal':
-      self.temp_file_object = open(self.temp_file_name, 'r')
-    return self
+    def __init__(self, dtype="random", zone=1):
+        self.zone = zone
+        self.dtype = dtype
 
-  def __exit__(self, type, value, traceback):
-    self._entered = False
-    self._close_thermal_file()
+    def __enter__(self):
+        self._entered = True
+        if self._dtype == "thermal":
+            self.temp_file_object = open(self.temp_file_name, "r")
+        return self
 
-  def _close_thermal_file(self):
-    try:
-      self.temp_file_object.close()
-    except Exception as e:
-      pass
-
-  def _update_thermal(self):
-    self.temp_file_name = f'/sys/class/thermal/thermal_zone{self._zone}/temp'
-    if hasattr(self.temp_file_object, 'closed'):
-      if self.temp_file_object.closed == False:
-        self.temp_file_object.close()
-    if self._entered == True:
-      self.temp_file_object = open(self.temp_file_name, 'r')
-
-  @property
-  def zone(self):
-    return self._zone
-
-  @zone.setter
-  def zone(self, value):
-    if value != self._zone:
-      self._zone = value
-      self._update_thermal()
-
-  @property
-  def dtype(self):
-    return self._dtype
-
-  @dtype.setter
-  def dtype(self, value):
-    if value != self._dtype:
-      if value == 'thermal':
-        self._dtype = value
-        self._update_thermal()
-      elif value == 'random':
-        self._dtype = value
+    def __exit__(self, type, value, traceback):
+        self._entered = False
         self._close_thermal_file()
-      else:
-        print("Warning: Unknown datatype")
 
-  @property
-  def get(self):
-    if self._dtype == 'thermal':
-      try:
-        point_int = int(self.temp_file_object.readline())
-        self.temp_file_object.seek(0)
-      except:
-        point_int = float("nan")
-    elif self._dtype == "random":
-      point_int = random.randint(0, 100 * 1000)
-    else:
-      point_int = 0
-    return point_int / 1000
+    def _close_thermal_file(self):
+        try:
+            self.temp_file_object.close()
+        except Exception as e:
+            pass
 
-  @property
-  def thermaltype(self):
-    if self._dtype == 'thermal':
-      try:
-        type_file = f'/sys/class/thermal/thermal_zone{self._zone}/type'
-        with open(type_file, 'r') as fh:
-          type_str = fh.readline()
-        result = type_str.strip()
-      except Exception as e:
-        result = "Unknown"
-    elif self._dtype == 'random':
-      result = 'Random'
-    else:
-      result = "None"
-    return result
+    def _update_thermal(self):
+        self.temp_file_name = f"/sys/class/thermal/thermal_zone{self._zone}/temp"
+        if hasattr(self.temp_file_object, "closed"):
+            if self.temp_file_object.closed == False:
+                self.temp_file_object.close()
+        if self._entered == True:
+            self.temp_file_object = open(self.temp_file_name, "r")
+
+    @property
+    def zone(self):
+        return self._zone
+
+    @zone.setter
+    def zone(self, value):
+        if value != self._zone:
+            self._zone = value
+            self._update_thermal()
+
+    @property
+    def dtype(self):
+        return self._dtype
+
+    @dtype.setter
+    def dtype(self, value):
+        if value != self._dtype:
+            if value == "thermal":
+                self._dtype = value
+                self._update_thermal()
+            elif value == "random":
+                self._dtype = value
+                self._close_thermal_file()
+            else:
+                print("Warning: Unknown datatype")
+
+    @property
+    def get(self):
+        if self._dtype == "thermal":
+            try:
+                point_int = int(self.temp_file_object.readline())
+                self.temp_file_object.seek(0)
+            except:
+                point_int = float("nan")
+        elif self._dtype == "random":
+            point_int = random.randint(0, 100 * 1000)
+        else:
+            point_int = 0
+        return point_int / 1000
+
+    @property
+    def thermaltype(self):
+        if self._dtype == "thermal":
+            try:
+                type_file = f"/sys/class/thermal/thermal_zone{self._zone}/type"
+                with open(type_file, "r") as fh:
+                    type_str = fh.readline()
+                result = type_str.strip()
+            except Exception as e:
+                result = "Unknown"
+        elif self._dtype == "random":
+            result = "Random"
+        else:
+            result = "None"
+        return result
 
 
 class Downsampler(object):
-  """
-  Feed this class high frequency data and every [factor]
-  samples it will return an average of the last [factor] samples
-  Can be used as an input filter to slow down datarate (and potentially increase precision)
-  """
-  factor = None
-  cache = None
-  next_sample = None
+    """
+    Feed this class high frequency data and every [factor]
+    samples it will return an average of the last [factor] samples
+    Can be used as an input filter to slow down datarate (and potentially increase precision)
+    """
 
-  def __init__(self, factor=5):
-    self.factor = factor
-    self.cache = []
-    self.next_sample = 0
+    factor = None
+    cache = None
+    next_sample = None
 
-  def feed(self, sample):
-    self.next_sample += 1
-    self.cache.append(sample)
+    def __init__(self, factor=5):
+        self.factor = factor
+        self.cache = []
+        self.next_sample = 0
 
-    if self.next_sample == self.factor:  # the cache is full, compute and return the average
-      ret_val = sum(self.cache) / float(self.factor)
-      self.next_sample = 0
-      self.cache = []
-    else:
-      ret_val = None
+    def feed(self, sample):
+        self.next_sample += 1
+        self.cache.append(sample)
 
-    return ret_val
+        if self.next_sample == self.factor:  # the cache is full, compute and return the average
+            ret_val = sum(self.cache) / float(self.factor)
+            self.next_sample = 0
+            self.cache = []
+        else:
+            ret_val = None
+
+        return ret_val
--- src/livechart/ncurses_viewer.py
+++ src/livechart/ncurses_viewer.py
@@ -12,105 +12,109 @@ from livechart.lib import Downsampler
 
 
 def do_curse(stdscr, dtype="random", zone=1):
-  # Clear screen
-  stdscr.clear()  # clear the screen
-  stdscr.nodelay(True)  # don't wait for input
-  curses.curs_set(0)  # hide the cursor
-  thermal_zone_number = zone
+    # Clear screen
+    stdscr.clear()  # clear the screen
+    stdscr.nodelay(True)  # don't wait for input
+    curses.curs_set(0)  # hide the cursor
+    thermal_zone_number = zone
 
-  quit_key = 'q'
+    quit_key = "q"
 
-  delay = 0.001
+    delay = 0.001
 
-  # in characters
-  plot_width = 100
-  plot_height = 30
-  # TODO: read the terminal size with curses and use that
+    # in characters
+    plot_width = 100
+    plot_height = 30
+    # TODO: read the terminal size with curses and use that
 
-  average_window_length = round(plot_width / 5)  # length of running average window
-  downsample_by = 30  # factor for downsampling
+    average_window_length = round(plot_width / 5)  # length of running average window
+    downsample_by = 30  # factor for downsampling
 
-  display = deque([], plot_width)  # what we'll be displaying
+    display = deque([], plot_width)  # what we'll be displaying
 
-  cache = deque()  # used in calculating the rolling mean
-  cum_sum = 0  # used for calculating rolling mean
-  ds = Downsampler(downsample_by)
+    cache = deque()  # used in calculating the rolling mean
+    cum_sum = 0  # used for calculating rolling mean
+    ds = Downsampler(downsample_by)
 
-  with Datagetter(dtype=dtype, zone=thermal_zone_number) as dg:
-    tmp_type = dg.thermaltype
+    with Datagetter(dtype=dtype, zone=thermal_zone_number) as dg:
+        tmp_type = dg.thermaltype
 
-    while True:
-      stdscr.erase()
+        while True:
+            stdscr.erase()
 
-      #this_data = get_datapoint()
-      raw_data = dg.get  # get a new datapoint
-      while (this_data := ds.feed(raw_data)) is None:  # feed the downsampler with raw data until it gives us a data point
-        time.sleep(delay)
-        raw_data = dg.get  # get a new datapoint
+            # this_data = get_datapoint()
+            raw_data = dg.get  # get a new datapoint
+            while (this_data := ds.feed(raw_data)) is None:  # feed the downsampler with raw data until it gives us a data point
+                time.sleep(delay)
+                raw_data = dg.get  # get a new datapoint
 
-      # do rolling average computation
-      cache.append(this_data)
-      cum_sum += this_data
-      if len(cache) < average_window_length:
-        pass
-      else:
-        cum_sum -= cache.popleft()
-      this_avg = cum_sum / float(len(cache))
+            # do rolling average computation
+            cache.append(this_data)
+            cum_sum += this_data
+            if len(cache) < average_window_length:
+                pass
+            else:
+                cum_sum -= cache.popleft()
+            this_avg = cum_sum / float(len(cache))
 
-      # draw the plot
-      to_display = this_avg
-      #to_display = this_data
-      stdscr.addstr(0, 0, f"{tmp_type} Temperature = {to_display:.2f}°C     ===== press {quit_key} to quit =====")
-      display.append(to_display)
-      stdscr.addstr(1, 0, asciichartpy.plot(display, {'height': plot_height}))
-      stdscr.refresh()
+            # draw the plot
+            to_display = this_avg
+            # to_display = this_data
+            stdscr.addstr(
+                0,
+                0,
+                f"{tmp_type} Temperature = {to_display:.2f}°C     ===== press {quit_key} to quit =====",
+            )
+            display.append(to_display)
+            stdscr.addstr(1, 0, asciichartpy.plot(display, {"height": plot_height}))
+            stdscr.refresh()
 
-      ch = stdscr.getch()
-      if ch == ord(quit_key):  # q key ends the program
-        break
-      elif ch == ord('r'):  # r key does nothing
-        pass
+            ch = stdscr.getch()
+            if ch == ord(quit_key):  # q key ends the program
+                break
+            elif ch == ord("r"):  # r key does nothing
+                pass
 
 
 def main():
-  # this will be hardware dependent and might need to be changed for different systems to find the CPU
-  # the following is very helpful!
-  # paste <(ls /sys/class/thermal/ | grep thermal_zone) <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
-  if 'ix' in os.name:
-    if len(sys.argv) > 1:
-      thermal_zone_number = int(sys.argv[1])
-      dtype = "thermal"
+    # this will be hardware dependent and might need to be changed for different systems to find the CPU
+    # the following is very helpful!
+    # paste <(ls /sys/class/thermal/ | grep thermal_zone) <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+    if "ix" in os.name:
+        if len(sys.argv) > 1:
+            thermal_zone_number = int(sys.argv[1])
+            dtype = "thermal"
+        else:
+            print("Enter 1 for random data or 0 for thermal data [1]: ", end="")
+            user = input()
+            if user == "":
+                user = "1"
+                print(user)
+
+            if user == "0":
+                dtype = "thermal"
+            elif user == "1":
+                dtype = "random"
+            else:
+                raise ValueError(f"{user} is not 0 or 1")
+
+            if user == "0":
+                print()
+                os.system("bash -c \"paste <(ls /sys/class/thermal/ | grep thermal_zone) <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'\"")
+                print("Pick a thermal zone number to monitor [0-N]: ", end="")
+                user = input()
+                if user == "":
+                    user = "0"
+                    print(user)
+                thermal_zone_number = int(user)
+            else:
+                thermal_zone_number = 1
     else:
-      print('Enter 1 for random data or 0 for thermal data [1]: ', end='')
-      user = input()
-      if user == '':
-        user = '1'
-        print(user)
-
-      if user == '0':
-        dtype = "thermal"
-      elif user == '1':
-        dtype = "random"
-      else:
-        raise ValueError(f"{user} is not 0 or 1")
-
-      if user == '0':
-        print()
-        os.system("bash -c \"paste <(ls /sys/class/thermal/ | grep thermal_zone) <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'\"")
-        print('Pick a thermal zone number to monitor [0-N]: ', end='')
-        user = input()
-        if user == '':
-          user = '0'
-          print(user)
-        thermal_zone_number = int(user)
-      else:
         thermal_zone_number = 1
-  else:
-    thermal_zone_number = 1
-    dtype = "random"
+        dtype = "random"
 
-  curses.wrapper(do_curse, dtype=dtype, zone=thermal_zone_number)
+    curses.wrapper(do_curse, dtype=dtype, zone=thermal_zone_number)
 
 
 if __name__ == "__main__":
-  main()
+    main()
